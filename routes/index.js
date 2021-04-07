64c3eefdc4166aae91f47de9586dc48e3516aac9
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-07 19:21:01 +0530

    Controllers & displaying first page

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+let homeController = require('../controllers/home_controllers');
+
+router.get('/', homeController.home);
 
 module.exports = router;
